e811fc3e52c8883d63cb5bfd4ab610d75a88b894
slavinto <slava3669@gmail.com>
2023-01-11 17:16:32 +0300

    Lesson124 implemented login

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/error-middleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
+// connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+// routes
+app.use("/api/users", require("./routes/user-routes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
