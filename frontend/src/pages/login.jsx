ea538a3f2aad5f995d1839e065cc0bf29b42a9ec
slavinto <slava3669@gmail.com>
2023-01-23 16:45:56 +0300

    Lesson131 implementing register user

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { FaSignInAlt } from "react-icons/fa";
 import { toast } from "react-toastify";
+import { login } from "../features/auth/auth-slice";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -9,6 +11,11 @@ const Login = () => {
   });
 
   const { email, password } = formData;
+
+  const dispatch = useDispatch();
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -19,6 +26,13 @@ const Login = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
 
   return (
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
-import { register } from "../features/auth/auth-slice";
+import { register, reset } from "../features/auth/auth-slice";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,10 +15,23 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData;
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const { user, isLoading, isSuccess, message } = useSelector(
+  const { user, isError, isLoading, isSuccess, message } = useSelector(
     (state) => state.auth
   );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -30,6 +44,14 @@ const Register = () => {
     e.preventDefault();
     if (password !== password2) {
       toast.error("Error. Passwords do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
     }
   };
 
@@ -37,7 +59,7 @@ const Register = () => {
     <>
       <section className="heading">
         <h1>
-          <FaUser /> Register {user}
+          <FaUser /> Register
         </h1>
         <p>Please create an account</p>
       </section>
